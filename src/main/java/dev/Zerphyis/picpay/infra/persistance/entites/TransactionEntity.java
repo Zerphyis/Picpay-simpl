c6e8435a2b6d460ea67efebba48ec99a37ed37cf
Otavio <estudosprogotavio@gmail.com>
2026-01-13 06:42:05 -0300

    refactor:mudando atributos da entidade TransactionEntity

--- src/main/java/dev/Zerphyis/picpay/infra/persistance/entites/TransactionEntity.java
+++ src/main/java/dev/Zerphyis/picpay/infra/persistance/entites/TransactionEntity.java
@@ -1,9 +1,7 @@
 package dev.Zerphyis.picpay.infra.persistance.entites;
 
-import dev.Zerphyis.picpay.domain.entities.users.Users;
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.data.annotation.Id;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -21,25 +19,15 @@ public class TransactionEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(optional = false)
-    @JoinColumn(
-            name = "sender_id",
-            nullable = false,
-            foreignKey = @ForeignKey(name = "fk_transaction_sender")
-    )
-    private Users sender;
+    @Column(name = "sender_id", nullable = false)
+    private Long senderId;
 
-    @ManyToOne(optional = false)
-    @JoinColumn(
-            name = "receiver_id",
-            nullable = false,
-            foreignKey = @ForeignKey(name = "fk_transaction_receiver")
-    )
-    private Users receiver;
+    @Column(name = "receiver_id", nullable = false)
+    private Long receiverId;
 
     @Column(nullable = false, precision = 19, scale = 2)
     private BigDecimal value;
 
-    @Column(name = "created_time", nullable = false)
-    private LocalDateTime createdTime;
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
 }
