05ddf9f80a0304ce52bcf80ed10f4d934c61e328
Otavio <estudosprogotavio@gmail.com>
2026-01-13 06:44:02 -0300

    feat:adicionando beans de UseCases

---
+++ src/main/java/dev/Zerphyis/picpay/infra/configs/UseCasesConfig.java
@@ -0,0 +1,37 @@
+package dev.Zerphyis.picpay.infra.configs;
+
+import dev.Zerphyis.picpay.aplication.usecases.*;
+import dev.Zerphyis.picpay.domain.interfaceCases.*;
+import dev.Zerphyis.picpay.domain.repositories.UserGateway;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class UseCasesConfig {
+
+    @Bean
+    public CreateUserInterface createUserUseCase(UserGateway userGateway) {
+        return new CreateUsersImpl(userGateway);
+    }
+
+    @Bean
+    public VerifyUserInterface verifyUserUseCase(UserGateway userGateway) {
+        return new VerifyUsersImpl(userGateway);
+    }
+
+
+    @Bean
+    public TransferValidationInterface transferValidationUseCase() {
+        return new TransferValidationImpl();
+    }
+
+    @Bean
+    public BalanceValidateInterface balanceValidateUseCase() {
+        return new BalanceValidateImpl();
+    }
+
+    @Bean
+    public ValidateMechantUser validateMerchantUserUseCase() {
+        return new ValidateMerchantUserImpl();
+    }
+}
