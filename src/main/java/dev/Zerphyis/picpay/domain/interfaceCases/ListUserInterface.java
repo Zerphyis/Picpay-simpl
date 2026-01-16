e3ef80200f9d3987c924b9589a2aa6edf3f99c53
Otavio <estudosprogotavio@gmail.com>
2026-01-16 10:37:25 -0300

    feat:adicionando interface ListUserInterface

---
+++ src/main/java/dev/Zerphyis/picpay/domain/interfaceCases/ListUserInterface.java
@@ -0,0 +1,9 @@
+package dev.Zerphyis.picpay.domain.interfaceCases;
+
+import dev.Zerphyis.picpay.domain.entities.users.Users;
+
+import java.util.List;
+
+public interface ListUserInterface {
+    List<Users> execute();
+}
