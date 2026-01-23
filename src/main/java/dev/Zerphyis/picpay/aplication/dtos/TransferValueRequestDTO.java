edf02cc233bdbada306ff059125eee3dbd7afd28
Otavio <estudosprogotavio@gmail.com>
2026-01-23 11:00:20 -0300

    refactor:corrigindo TransferValueRequestDTO

--- src/main/java/dev/Zerphyis/picpay/aplication/dtos/TransferValueRequestDTO.java
+++ src/main/java/dev/Zerphyis/picpay/aplication/dtos/TransferValueRequestDTO.java
@@ -2,5 +2,5 @@ package dev.Zerphyis.picpay.aplication.dtos;
 
 import java.math.BigDecimal;
 
-public record TransferValueRequestDTO(Long PayerId, Long PaayerId, BigDecimal value) {
+public record TransferValueRequestDTO(Long PayerId, Long PayeeId, BigDecimal value) {
 }
