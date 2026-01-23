d9ab00b3fa12bc11a756dffe1a3662984f50ac01
Otavio <estudosprogotavio@gmail.com>
2026-01-23 10:58:01 -0300

    refactor:corrigindo Dto CreateUserRequestDTO

--- src/main/java/dev/Zerphyis/picpay/aplication/dtos/CreateUserRequestDTO.java
+++ src/main/java/dev/Zerphyis/picpay/aplication/dtos/CreateUserRequestDTO.java
@@ -1,10 +1,16 @@
 package dev.Zerphyis.picpay.aplication.dtos;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.math.BigDecimal;
+
 public record CreateUserRequestDTO(
-        String fullName,
+        String fullname,
+
         String document,
         String email,
         String password,
+        BigDecimal balance,
         String userType
 ) {
 }
