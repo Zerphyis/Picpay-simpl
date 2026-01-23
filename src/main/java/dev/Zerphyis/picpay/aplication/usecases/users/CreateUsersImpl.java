7b8e3c3b83ef4c76bc22dddd12f928de8d1cd9b5
Otavio <estudosprogotavio@gmail.com>
2026-01-23 10:56:38 -0300

    refactor:movendo CreateUsersImpl movendo para novo pacote users em usecases

--- src/main/java/dev/Zerphyis/picpay/aplication/usecases/users/CreateUsersImpl.java
+++ src/main/java/dev/Zerphyis/picpay/aplication/usecases/users/CreateUsersImpl.java
@@ -1,12 +1,10 @@
-package dev.Zerphyis.picpay.aplication.usecases;
+package dev.Zerphyis.picpay.aplication.usecases.users;
 
 import dev.Zerphyis.picpay.aplication.dtos.CreateUserRequestDTO;
 import dev.Zerphyis.picpay.domain.entities.users.Users;
 import dev.Zerphyis.picpay.domain.entities.users.UsersType;
 import dev.Zerphyis.picpay.domain.interfaceCases.CreateUserInterface;
 import dev.Zerphyis.picpay.domain.repositories.UserGateway;
-
-import java.math.BigDecimal;
 
 public class CreateUsersImpl implements CreateUserInterface {
 
@@ -23,14 +21,12 @@ public class CreateUsersImpl implements CreateUserInterface {
         UsersType userType = UsersType.fromString(dto.userType());
 
         Users user = new Users();
-        user.setFullname(dto.fullName());
+        user.setFullname(dto.fullname());
+        user.setBalance(dto.balance());
         user.setDocument(dto.document());
         user.setEmail(dto.email());
         user.setPassword(dto.password());
         user.setUserType(userType);
-
-
-        user.setBalance(BigDecimal.ZERO);
 
 
         return repository.save(user);
