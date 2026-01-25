e87887185654c6b29ab0c02e6b344b684b11fadb
Otavio <estudosprogotavio@gmail.com>
2026-01-25 05:14:57 -0300

    refactor:alterando atributos em Transaction

--- src/main/java/dev/Zerphyis/picpay/domain/entities/transfers/Transaction.java
+++ src/main/java/dev/Zerphyis/picpay/domain/entities/transfers/Transaction.java
@@ -1,39 +1,30 @@
 package dev.Zerphyis.picpay.domain.entities.transfers;
 
+import dev.Zerphyis.picpay.domain.entities.users.Users;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
 public class Transaction {
 
-    private final Long id;
-    private final Long senderId;
-    private final Long receiverId;
-    private final BigDecimal value;
+    private Long id;
+    private BigDecimal amount;
+    private Users sender;
+    private Users receiver;
     private final LocalDateTime createdAt;
-
-
-    public Transaction(Long senderId, Long receiverId, BigDecimal value) {
-        this(null, senderId, receiverId, value, LocalDateTime.now());
-    }
-
 
     public Transaction(
             Long id,
-            Long senderId,
-            Long receiverId,
-            BigDecimal value,
-            LocalDateTime createdAt
+            BigDecimal amount,
+            Users sender,
+            Users receiver
     ) {
         this.id = id;
-        this.senderId = Objects.requireNonNull(senderId, "senderId não pode ser nulo");
-        this.receiverId = Objects.requireNonNull(receiverId, "receiverId não pode ser nulo");
-        this.value = Objects.requireNonNull(value, "value não pode ser nulo");
-        this.createdAt = Objects.requireNonNull(createdAt, "createdAt não pode ser nulo");
-
-        if (value.signum() <= 0) {
-            throw new IllegalArgumentException("O valor da transação deve ser maior que zero");
-        }
+        this.amount = Objects.requireNonNull(amount, "amount não pode ser nulo");
+        this.sender = Objects.requireNonNull(sender, "sender não pode ser nulo");
+        this.receiver = Objects.requireNonNull(receiver, "receiver não pode ser nulo");
+        this.createdAt = LocalDateTime.now();
     }
 
 
@@ -41,19 +32,36 @@ public class Transaction {
         return id;
     }
 
-    public Long getSenderId() {
-        return senderId;
+    public BigDecimal getAmount() {
+        return amount;
     }
 
-    public Long getReceiverId() {
-        return receiverId;
+    public Users getSender() {
+        return sender;
     }
 
-    public BigDecimal getValue() {
-        return value;
+    public Users getReceiver() {
+        return receiver;
     }
 
     public LocalDateTime getCreatedAt() {
         return createdAt;
+    }
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = Objects.requireNonNull(amount, "amount não pode ser nulo");
+    }
+
+    public void setSender(Users sender) {
+        this.sender = Objects.requireNonNull(sender, "sender não pode ser nulo");
+    }
+
+    public void setReceiver(Users receiver) {
+        this.receiver = Objects.requireNonNull(receiver, "receiver não pode ser nulo");
     }
 }
