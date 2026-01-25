d2f8ac0b18a6b2ab76d3e848f465aea78ac32e85
Otavio <estudosprogotavio@gmail.com>
2026-01-25 05:00:15 -0300

    feat:adicionando cronstrutor em UsersEntity

--- src/main/java/dev/Zerphyis/picpay/infra/persistance/entites/UsersEntity.java
+++ src/main/java/dev/Zerphyis/picpay/infra/persistance/entites/UsersEntity.java
@@ -1,20 +1,22 @@
 package dev.Zerphyis.picpay.infra.persistance.entites;
 
-
 import dev.Zerphyis.picpay.domain.entities.users.UsersType;
 import jakarta.persistence.*;
 import lombok.*;
-import jakarta.persistence.Id;
-
 
 import java.math.BigDecimal;
 
 @Entity
-@Table(name = "users")
+@Table(
+        name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "uk_users_document", columnNames = "document"),
+                @UniqueConstraint(name = "uk_users_email", columnNames = "email")
+        }
+)
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EqualsAndHashCode(of = "id")
 public class UsersEntity {
 
@@ -25,21 +27,35 @@ public class UsersEntity {
     @Column(nullable = false)
     private String fullname;
 
-    @Column(nullable = false, unique = true, length = 14)
+    @Column(nullable = false, length = 14)
     private String document;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String email;
 
     @Column(nullable = false)
     private String password;
 
-    @Column(nullable = false)
+    @Column(nullable = false, precision = 19, scale = 2)
     private BigDecimal balance;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "user_type", nullable = false)
+    @Column(name = "user_type", nullable = false, length = 20)
     private UsersType usersType;
 
-
+    public UsersEntity(
+            String fullname,
+            String document,
+            String email,
+            String password,
+            BigDecimal balance,
+            UsersType usersType
+    ) {
+        this.fullname = fullname;
+        this.document = document;
+        this.email = email;
+        this.password = password;
+        this.balance = balance;
+        this.usersType = usersType;
+    }
 }
