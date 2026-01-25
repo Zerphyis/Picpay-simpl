dd2cd1013073aff6f7028964e9ad5490fe3b268c
Otavio <estudosprogotavio@gmail.com>
2026-01-25 05:15:26 -0300

    refactor:corrigindo metodos em TransactionMapper

--- src/main/java/dev/Zerphyis/picpay/infra/persistance/mappers/TransactionMapper.java
+++ src/main/java/dev/Zerphyis/picpay/infra/persistance/mappers/TransactionMapper.java
@@ -1,30 +1,33 @@
 package dev.Zerphyis.picpay.infra.persistance.mappers;
 
 import dev.Zerphyis.picpay.domain.entities.transfers.Transaction;
+import dev.Zerphyis.picpay.domain.entities.users.Users;
 import dev.Zerphyis.picpay.infra.persistance.entites.TransactionEntity;
+import dev.Zerphyis.picpay.infra.persistance.mappers.UsersMapper;
 
 public class TransactionMapper {
 
-    private TransactionMapper() {
+    public static Transaction toDomain(TransactionEntity entity) {
+        Users sender = UsersMapper.toDomain(entity.getSenderId());
+        Users receiver = UsersMapper.toDomain(entity.getReceiverId());
+
+        return new Transaction(
+                entity.getId(),
+                entity.getValue(),
+                sender,
+                receiver
+        );
     }
 
     public static TransactionEntity toEntity(Transaction transaction) {
-        return new TransactionEntity(
-                transaction.getId(),
-                transaction.getSenderId(),
-                transaction.getReceiverId(),
-                transaction.getValue(),
-                transaction.getCreatedAt()
-        );
-    }
+        TransactionEntity entity = new TransactionEntity();
 
-    public static Transaction toDomain(TransactionEntity entity) {
-        return new Transaction(
-                entity.getId(),
-                entity.getSenderId(),
-                entity.getReceiverId(),
-                entity.getValue(),
-                entity.getCreatedAt()
-        );
+        entity.setId(transaction.getId());
+        entity.setValue(transaction.getAmount());
+        entity.setSenderId(UsersMapper.toEntity(transaction.getSender()));
+        entity.setReceiverId(UsersMapper.toEntity(transaction.getReceiver()));
+        entity.setCreatedAt(transaction.getCreatedAt());
+
+        return entity;
     }
 }
