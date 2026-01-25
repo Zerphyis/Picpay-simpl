709114b4d94b394f42c2f71845bebaad69ffc055
Otavio <estudosprogotavio@gmail.com>
2026-01-25 04:59:38 -0300

    refactor:corrigindo migration V2

--- src/main/resources/db/migration/V2__create_transactions_table.sql
+++ src/main/resources/db/migration/V2__create_transactions_table.sql
@@ -1,12 +1,12 @@
 CREATE TABLE transactions
 (
-    id           BIGINT AUTO_INCREMENT PRIMARY KEY,
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
 
-    sender_id    BIGINT         NOT NULL,
-    receiver_id  BIGINT         NOT NULL,
+    sender_id   BIGINT         NOT NULL,
+    receiver_id BIGINT         NOT NULL,
 
-    amount       DECIMAL(19, 2) NOT NULL,
-    created_time TIMESTAMP      NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    value       DECIMAL(19, 2) NOT NULL,
+    created_at  TIMESTAMP      NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT fk_transaction_sender
         FOREIGN KEY (sender_id)
@@ -16,6 +16,7 @@ CREATE TABLE transactions
         FOREIGN KEY (receiver_id)
             REFERENCES users (id),
 
-    CONSTRAINT chk_transaction_amount_positive
-        CHECK (amount > 0)
+    CONSTRAINT chk_transaction_value_positive
+        CHECK (value > 0)
 ) ENGINE=InnoDB;
+    
