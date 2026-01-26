ec9dc5dca6ac6dd4e99ec51b7341eab9484f1f22
Otavio <estudosprogotavio@gmail.com>
2026-01-26 03:28:51 -0300

    feat:adicionando listall e refund em TransferController

--- src/main/java/dev/Zerphyis/picpay/infra/controllers/TransferController.java
+++ src/main/java/dev/Zerphyis/picpay/infra/controllers/TransferController.java
@@ -2,13 +2,13 @@ package dev.Zerphyis.picpay.infra.controllers;
 
 import dev.Zerphyis.picpay.aplication.dtos.TransferValueRequestDTO;
 import dev.Zerphyis.picpay.aplication.usecases.transfer.TransferService;
+import dev.Zerphyis.picpay.domain.entities.transfers.Transaction;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/transfers")
@@ -30,5 +30,18 @@ public class TransferController {
                 request.value()
         );
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Transaction>> listAll() {
+        return ResponseEntity.ok(transferService.listAllTransactions());
+    }
+
+    @PostMapping("/{transactionId}/refund")
+    public ResponseEntity<Void> refund(
+            @PathVariable Long transactionId
+    ) {
+        transferService.refund(transactionId);
+        return ResponseEntity.noContent().build();
     }
 }
